1217c6712d2d0f805c93a3bffc34524590d6bf9e
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-22 20:05:07 -0800

    m1?

--- indexer.py
+++ indexer.py
@@ -3,20 +3,17 @@ from bs4 import BeautifulSoup
 import re
 import json
 import lxml
+import os
 
-doc_id = {}
+doc_id = []
 inverted_index = {}
 porter = PorterStemmer()
 
-def posting_dict(doc_id: int, tf: float) -> dict:
-    return {"doc_id": doc_id, "tf": tf}
-
 # returns a list of tf's for each word in the document
 def process_words(document: str) -> dict:
-    document = document.replace('\n', '').replace('\t', '')
+    document = re.sub(r'[^A-Za-z0-9]+', ' ', document).replace('\n', ' ').replace('\t', ' ').replace('\r',' ').lower().split()
     num_words = len(document)
-    stemmed_words = [porter.stem(re.sub(r'[^A-Za-z0-9]+', '', word)) for word in document]
-
+    stemmed_words = [porter.stem(word) for word in document]
     tf = {}
     for word in stemmed_words:
         if word in tf: tf[word] += 1 
@@ -33,10 +30,42 @@ def parse_json(path):
     # file_content = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'a', 'title', 'span'], text=True)
     # file_content = ' '.join([e.string for e in file_content])
     # file_content = ' '.join([e.string for e in soup.recursiveChildGenerator() if isinstance(e, str)])
-    for tag in soup.find_all('script'):
+    for tag in soup.find_all(['script', 'style']):
         tag.extract()
-    file_content = soup.get_text()
+    file_content = soup.get_text(" ")
     return file_content
 
+def process_directory(domain: str):
+    os.chdir(os.getcwd() + "/" + domain)
+    for site in os.listdir(os.getcwd()):
+        current_id = len(doc_id)
+        doc_id.append({'id': current_id, 'url': site})
+        words_file = parse_json(site)
+        tf_dict = process_words(words_file)
+        process_tf_dict(tf_dict, current_id)
+    os.chdir('..')
 
-print(parse_json("DEV/aiclub_ics_uci_edu/8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json"))
+def process_tf_dict(tf: dict, doc_id: int):
+    for word in tf:
+        if word in inverted_index: inverted_index[word].append(posting_dict(doc_id, tf[word]))
+        else: inverted_index[word] = [posting_dict(doc_id, tf[word])]
+
+def posting_dict(doc_id: int, tf: float) -> dict:
+    return {"doc_id": doc_id, "tf": tf}
+
+def process_dev():
+    os.chdir("/Users/kingsleyszeto/Documents/GitHub/cs121-ast3/DEV")
+    for f in os.listdir(os.getcwd()):
+        if os.path.isdir(f): process_directory(f)
+        break
+def clean_print():
+    for word in inverted_index:
+        print(word)
+        for posting in inverted_index[word]:
+            print('\t', end = "")
+            print(posting)
+
+process_dev()
+clean_print()
+a = sorted(inverted_index.keys(), key=lambda x: len(x))[-1]
+print(a)
